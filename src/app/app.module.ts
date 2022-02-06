62e5870d0d7bdcab4e4aa4e11851e070556c5b2c
rsh456 <rsh456@hotmail.com>
2022-02-06 15:28:09 -0500

    Data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { ProductListComponent } from './products/product-list.component';
     ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   bootstrap: [AppComponent]
 })
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -5,7 +5,11 @@ import { Component } from "@angular/core";
     templateUrl: './product-list.component.html'
 })
 export class ProductListComponent{
-    pageTitle: string = 'Product List!';
+    pageTitle:string = 'Product List!';
+    imageWidth:number = 50;
+    imageMargin:number = 2;
+    showImage:boolean=false;
+    listFilter: string = 'cart';
     products: any[] = [
       {
         "productId": 2,
@@ -28,5 +32,7 @@ export class ProductListComponent{
         "imageUrl": "assets/images/hammer.png"
       }
     ];
-  
+  toggleImage():void{
+    this.showImage=!this.showImage;
+  }
 }
