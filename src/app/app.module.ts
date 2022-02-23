71c4356ebc2b793d65763bc0c5cd8eef73d63d50
rsh456 <rsh456@hotmail.com>
2022-02-23 08:14:27 -0500

    add navigation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StarComponent } from './shared/star.component';
 import {HttpClientModule} from '@angular/common/http';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,14 @@ import { WelcomeComponent } from './home/welcome.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path:'products', component: ProductListComponent},
+      {path:'products/:id', component: ProductDetailComponent},
+      {path:'welcome', component: WelcomeComponent},
+      {path:'',redirectTo:'welcome', pathMatch:'full' },
+      {path:'**',redirectTo:'welcome', pathMatch:'full' } 
+    ])
   ],
   bootstrap: [AppComponent]
 })
